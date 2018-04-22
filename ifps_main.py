4763f378493a503b185307392f59e45083195819
User 2 <dds2412@mail.ru>
2018-04-22 13:16:46 +0300

    sadasd

--- ifps_main.py
+++ ifps_main.py
@@ -2,13 +2,12 @@ import ipfsapi
 import  json
 
 def from_ipfs_to_hash(hash34):
-    return hash34.encode('utf-8')[2:]
+    return hash34[1:len(hash34)]
 
 
 def from_hash_to_ipfs(hash32):
-    hash32.insert(0, 109)
-    hash32.insert(0, 81)
-    return hash32.encode('utf-8')
+    
+    return chr(81) + chr(109)  + hash32
 
 
 def connection_to_ipfs_net():
@@ -20,12 +19,14 @@ def send_to_ipfs(json_f):
     with open('data.txt','w') as outline:
         json.dump(json_f, outline)
 
-    res = api.add(outline)
-    return from_ipfs_to_hash(res['hash'])
+    res = api.add('data.txt')
+    
+    return from_ipfs_to_hash(res['Hash'])
 
 
 def get_from_ipfs(hash):
     transport_hash = from_hash_to_ipfs(hash)
+    api = connection_to_ipfs_net()
     return api.get(transport_hash)
 
 
