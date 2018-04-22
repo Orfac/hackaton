7314c8529a1b9cf841cf17f4366c605b34f2ee8e
User 2 <dds2412@mail.ru>
2018-04-22 08:49:01 +0300

    Fixed

--- ifps/ifps_main.py
+++ ifps/ifps_main.py
@@ -1,9 +1,10 @@
+import ipfsapi
+
 def from_ipfs_to_hash(hash34):
     byte32array = bytearray(hash34, 'utf-8')
     byte32array.pop(0)
     byte32array.pop(0)
     return list(byte32array)
-
 
 def from_hash_to_ipfs(hash32):
     byte34array = bytearray(hash32, 'utf-8')
@@ -11,16 +12,15 @@ def from_hash_to_ipfs(hash32):
     byte34array.insert(0, 109)
     return list(byte34array)
 
+def connection_to_ipfs_net():
+    return ipfsapi.connect('https://ipfs.infura.io', 5001)
 
-import ipfsapi
-api = ipfsapi.connect('https://ipfs.infura.io', 5001)
-res = api.add('test2.txt')
-b = res
-cd = api.get('QmNwBTB8VR1uYDNU4hnz4dwsGTWW5moeWAZSUczwrCy6r5')
-c = api.cat(res['Hash'])
-d = api.id()
-l = from_ipfs_to_hash(b['Hash'])
-print('asd')
+def send_to_ipfs(json_f):
+    api = connection_to_ipfs_net()
+    res = api.add(json_f)
+    return from_ipfs_to_hash(res['hash'])
 
+def get_from_ipfs(hash):
+   return api.get(hash)
 
 
--- main.py
+++ main.py
@@ -2,22 +2,13 @@ import ipfsapi
 from RestApi import *
 
 def main():
-    add_transaction("asdas","asdas","asda")
-    #api = ipfsapi.connect('https://ipfs.infura.io', 5001)
-    #res = api.add('Hello.txt')
-    #b = res
-    #cd = api.get('QmNwBTB8VR1uYDNU4hnz4dwsGTWW5moeWAZSUczwrCy6r5')
-    #c = api.cat(res['Hash'])
-    
-    #d = api.id()
-    #print(c)
-    #print(d)
-    ##cd = api.get()
-  
-    #dq = did()
-    #dq
-    print()
+    #add_transaction("asdas","asdas","asda")
+    api = ipfsapi.connect('https://ipfs.infura.io', 5001)
+    res = api.add('hello.txt')
+    b = res
+    cd = api.get('QmNwBTB8VR1uYDNU4hnz4dwsGTWW5moeWAZSUczwrCy6r5')
+    d = api.id()
+    print(d)
 
 if __name__ == "__main__":
     main()
-    		
